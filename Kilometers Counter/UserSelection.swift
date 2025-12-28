daf1f0d69769a4a0a9879644b1727f68dd429f8f
MAF3X <matteograncan@gmail.com>
2025-12-28 11:53:54 +0100

    prima parte aggiunta accounts e correzzioni per ricalcolo del totale da pagare

---
+++ Kilometers Counter/UserSelection.swift
@@ -0,0 +1,18 @@
+//
+//  UserSelection.swift
+//  Kilometers Counter
+//
+//  Created by matteograssellifontana on 28/12/25.
+//
+
+import SwiftUI
+
+struct UserSelection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserSelection()
+}
